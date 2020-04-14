0114059f790fca17bc7a5e7b9dae97f5cff7849f
Alexander_Prochnow <alexprochnow132@gmail.com>
2020-04-14 14:38:54 +0200

    Agent cleaned and commented

--- agent.py
+++ agent.py
@@ -1,60 +1,54 @@
 import os
-#import tensorflow as tf
-#from tensorflow_probability.python.distributions import Normal
 import gym
 import numpy as np
 import ray
 from memory import Memory
 
-# Now as parameter for Coordinator init
-#ENV = 'LunarLanderContinuous-v2'
-#ENV = 'CartPole-v1'
-
 @ray.remote
 class A2CAgent:
     def __init__(self, num_steps, env):
         self.env = gym.make(env)
-        self.obs_space_size = self.env.observation_space.shape[0]
-        self.action_space_size = 1 if self.env.action_space.shape == () else self.env.action_space.shape[0]
-        self.action_space_bounds = [0, self.env.action_space.n-1] if self.env.action_space.shape == () else [self.env.action_space.low[0], self.env.action_space.high[0]]
         self.num_steps = num_steps
-        self.finished = False
+        self.finished = Falgise
+
+        # environment parameters
+        self.obs_space_size = self.env.observation_space.shape[0]
+        self.action_space_size =  self.env.action_space.shape[0]
+        self.action_space_bounds = [self.env.action_space.low[0], self.env.action_space.high[0]]
+
+        # get initial state and initialize memory
         self.state = self.env.reset()
         self.memory = Memory(self.num_steps, self.obs_space_size, self.action_space_size)
 
-    def observe(self, s):
-        if s == 0:
+    def observe(self, t):
+        # reset memory for new episode
+        if t == 0:
             self.memory.reset(self.num_steps, self.obs_space_size, self.action_space_size)
 
+        # reset environment at the end of an episode
         if self.finished:
             self.state = self.env.reset()
 
         self.state = np.reshape(self.state, [1,self.obs_space_size])
 
-        #self.env.render()
         return self.state, self.finished
-        
-    def execute(self, action, s):
+
+    def execute(self, action, t):
 
         if self.finished:
             self.finished = False
 
-        action = np.clip(action, self.action_space_bounds[0], self.action_space_bounds[1]) 
-        
-        # round action if action space is discrete
-        #if type(self.env.action_space).__name__ == 'Discrete':
-        #    action = np.math.round(action)
-        #    action = tf.cast(action, tf.int32).numpy()
+        # clip action value if necessary to be within action space
+        action = np.clip(action, self.action_space_bounds[0], self.action_space_bounds[1])
 
-        next_state, reward, done, _ = self.env.step(action) 
-        self.memory.store(self.state, action, reward, done, s)
+        # perform action and store the resulting state and reward
+        next_state, reward, done, _ = self.env.step(action)
+        self.memory.store(self.state, action, reward, done, t)
         self.state = next_state
         self.finished = done
-        
-        
-        if s == (self.num_steps - 1):
+
+
+        if t == (self.num_steps - 1):
             return self.memory
         else:
             return None
-
-    
